214dfac7dda6bc19d86c310ab7c1c03db199b239
Ha-g <you@example.com>
2019-12-21 17:15:14 -0800

    challenge 3 completed

--- Script.js
+++ Script.js
@@ -26,3 +26,26 @@ if ( marksAvg > johnsAvg && marksAvg > marysAvg){
 } else if(marysAvg > marksAvg && marysAvg > johnsAvg){
     console.log('Mary has a higher average with ' + marysAvg + ' points per game')
 }
+
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = 20 /100
+        
+    } if ( bill >= 50 && bill <= 200){
+        percentage = (15 / 100)
+        
+    } if(bill > 200 ){
+        percentage = (10 / 100)
+    }
+    return percentage * bill
+}
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+tipCalculator(bills[1]), 
+tipCalculator(bills[2])]
+var finalValues = [bills[0] + tips[0],
+                bills[1] + tips[1],
+                bills[2] + tips[2]]
+console.log(finalValues);
+
